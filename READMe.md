a1d38aec8e414ea1778ed3127475fcfd2bc0c8d9
Willy Aguirre <marti1003@hotmail.com>
2013-03-11 10:52:52 -0500

    query criteria

--- READMe.md
+++ READMe.md
@@ -4,21 +4,26 @@ the name of the project is: SparkExample
 
 ##Run the example
 
-* cd SparkExample
-* mvn compile exec:java -Dexec.mainClass=com.mompany.sparkexample.App
+::
+	$ cd SparkExample
+	$ mvn compile exec:java -Dexec.mainClass=com.mompany.sparkexample.App
 
 ##Sparkjava and FreeMarker
 
-example route http://localhost:4567/welcome
+::
+	example route http://localhost:4567/welcome
 
 ##Mongodb
 
-* use course
-* db.hello.insert({name:"MongoDB"})
-* db.hello.findOne()
-* { "_id" : ObjectId("512ee599610c324def2c886d"), "name" : "MongoDB" }
+::
+	use course
+	db.hello.insert({name:"MongoDB"})
+	db.hello.findOne()
+	{ "_id" : ObjectId("512ee599610c324def2c886d"), "name" : "MongoDB" }
 
 ##Spark Routes
-* /
-* /test
-* /echo/cat
+
+::
+	/
+	/test
+	/echo/cat
--- src/main/java/com/mycompany/sparkexample/InsertTest.java
+++ src/main/java/com/mycompany/sparkexample/InsertTest.java
@@ -6,6 +6,8 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.QueryBuilder;
+
 import java.net.UnknownHostException;
 import java.util.Random;
 
@@ -19,42 +21,50 @@ public class InsertTest {
 
         MongoClient client = new MongoClient();
         DB courseDB = client.getDB("course");
-        DBCollection collection = courseDB.getCollection("insertTest");
+        DBCollection collection = courseDB.getCollection("findCriteriaTest");
 
         collection.drop();
-        
-        for(int i = 0; i < 10; i++){
-            collection.insert(new BasicDBObject("x", new Random().nextInt(100)));
+
+        for (int i = 0; i < 10; i++) {
+            collection.insert(new BasicDBObject("x", new Random().nextInt(2))
+                    .append("y", new Random().nextInt(100)));
         }
 
+        QueryBuilder builder = new QueryBuilder().start("x").is(0)
+                .and("y").greaterThan(10).lessThan(70);
+
+        DBObject query = new BasicDBObject("x", 0)
+                .append("y", new BasicDBObject("$gt", 10)
+                .append("$lt", 90));
+
         /*DBObject doc = new BasicDBObject("_id", new ObjectId()).append("x", 1);
-        DBObject doc2 = new BasicDBObject().append("x", 2);
+         DBObject doc2 = new BasicDBObject().append("x", 2);
 
-        System.out.println(doc); 
+         System.out.println(doc); 
 
-        collection.insert(Arrays.asList(doc, doc2));
+         collection.insert(Arrays.asList(doc, doc2));
 
-        System.out.println(doc);*/
+         System.out.println(doc);*/
 
-        System.out.println("Find one: ");
-        DBObject one = collection.findOne();
-        System.out.println(one);
-        
+        /*System.out.println("Find one: ");
+         DBObject one = collection.findOne();
+         System.out.println(one);*/
+
+        System.out.println("\nCount: ");
+        long count = collection.count(builder.get());
+        System.out.println(count);
+
         System.out.println("\nFind all: ");
-        DBCursor cursor = collection.find();
-        
+        DBCursor cursor = collection.find(builder.get());
+
         try {
-            while(cursor.hasNext()){
+            while (cursor.hasNext()) {
                 DBObject cur = cursor.next();
                 System.out.println(cur);
             }
         } finally {
             cursor.close();
         }
-        
-        System.out.println("\nCount: ");
-        long count = collection.count();
-        System.out.println(count);
-        
+
     }
 }
